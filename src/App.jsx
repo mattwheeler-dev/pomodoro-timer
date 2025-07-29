1ab060e27c936699c446e308aabea7a0b7446c53
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2025-07-28 18:58:55 -0700

    create starting components;

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 const App = () => {
-	return <></>;
+	return (
+		<>
+			<h1>Pomodoro Timer</h1>
+			<div>timer / session display</div>
+			<div>timer / session type</div>
+			<div>number of work sessions so far</div>
+		</>
+	);
 };
 
 export default App;
